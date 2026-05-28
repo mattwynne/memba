9400aec579a3abede85a7248df387bed67c5ffa8
Matt Wynne <matt@mattwynne.net>
2026-05-28 21:54:39 +0000

    Implement iteration task: Add  Memba.Membership.App  and  Memba.Membership.Router .
    Plan: docs/iterations/001-event-sourced-foundation/plan.md
    Todo: docs/iterations/001-event-sourced-foundation/todo.md
    
    Evidence files changed:
    docs/iterations/001-event-sourced-foundation/todo.md
    web/lib/memba/application.ex
    web/lib/memba/membership/app.ex
    web/lib/memba/membership/router.ex
    web/test/event_sourced_setup_test.exs
    web/test/memba/membership/app_test.exs
    
    Diff stat:
     .../001-event-sourced-foundation/todo.md           |  2 +-
     web/lib/memba/application.ex                       |  1 +
     web/lib/memba/membership/app.ex                    |  9 ++++++
     web/lib/memba/membership/router.ex                 |  9 ++++++
     web/test/event_sourced_setup_test.exs              |  5 +++-
     web/test/memba/membership/app_test.exs             | 32 ++++++++++++++++++++++
     6 files changed, 56 insertions(+), 2 deletions(-)

---
+++ web/test/memba/membership/app_test.exs
@@ -0,0 +1,32 @@
+defmodule Memba.Membership.AppTest do
+  use ExUnit.Case, async: true
+
+  import ExUnit.CaptureLog
+
+  alias Memba.Membership.App
+  alias Memba.Membership.Router
+
+  test "Membership Commanded app is supervised by the Phoenix application" do
+    assert is_pid(Process.whereis(App))
+    assert is_pid(Process.whereis(Memba.EventStore))
+
+    assert Enum.any?(Supervisor.which_children(Memba.Supervisor), fn
+             {App, pid, :supervisor, [App]} when is_pid(pid) -> true
+             _child -> false
+           end)
+  end
+
+  test "Membership Commanded app includes the Membership router" do
+    assert App.__registered_commands__() == Router.__registered_commands__()
+    assert Router.__registered_commands__() == []
+  end
+
+  test "Membership Commanded app dispatches through its router" do
+    log =
+      capture_log(fn ->
+        assert {:error, :unregistered_command} = App.dispatch(%URI{})
+      end)
+
+    assert log =~ "attempted to dispatch an unregistered command"
+  end
+end
