f82fdbd80a7e06278265a5bc41bc04ea20ec0255
Matt Wynne <matt@mattwynne.net>
2026-05-31 01:34:58 -0700

    Allow wildcard club subdomain origins

--- web/config/runtime.exs
+++ web/config/runtime.exs
@@ -105,6 +105,7 @@ if config_env() == :prod do
 
   config :memba, MembaWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
+    check_origin: ["//#{host}", "//*.#{host}"],
     http: [
       # Enable IPv6 and bind on all interfaces.
       # Set it to  {0, 0, 0, 0, 0, 0, 0, 1} for local network only access.
