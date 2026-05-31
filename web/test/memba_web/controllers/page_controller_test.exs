e4c5b1b280715d85df82abf37bf57ee30290720e
Matt Wynne <matt@mattwynne.net>
2026-05-31 01:56:32 -0700

    Add Memba homepage

--- web/test/memba_web/controllers/page_controller_test.exs
+++ web/test/memba_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule MembaWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, ~p"/")
-    assert html_response(conn, 200) =~ "Peace of mind from prototype to production"
+    assert html_response(conn, 200) =~ "Keep every member in the loop."
   end
 end
