3a1a064e5ca0cceb369937c409bcc4d384d87cc6
Matt Wynne <matt@mattwynne.net>
2026-05-31 11:45:10 -0700

    Redirect Fly hostname to canonical domain

--- web/lib/memba_web/endpoint.ex
+++ web/lib/memba_web/endpoint.ex
@@ -15,6 +15,8 @@ defmodule MembaWeb.Endpoint do
     websocket: [connect_info: [session: @session_options]],
     longpoll: [connect_info: [session: @session_options]]
 
+  plug MembaWeb.Plugs.CanonicalHostRedirect
+
   # Serve at "/" the static files from "priv/static" directory.
   #
   # When code reloading is disabled (e.g., in production),
