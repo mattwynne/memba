cc311e875678b0157fc91d3e32dba866d1647153
Fabro <noreply@fabro.sh>
2026-05-31 21:44:37 +0000

    fabro(01KSZZBQ0M243VTDR0VSZC0QR4): fix_dev_check (succeeded)
    Fabro-Run: 01KSZZBQ0M243VTDR0VSZC0QR4
    Fabro-Completed: 5
    Fabro-Checkpoint: fdcb6b0c1e4fe692aa75564fef128b2f16d77b27
    
    ⚒️ Generated with [Fabro](https://fabro.sh)

--- web/test/memba_web/plugs/canonical_host_redirect_test.exs
+++ web/test/memba_web/plugs/canonical_host_redirect_test.exs
@@ -1,6 +1,8 @@
 defmodule MembaWeb.Plugs.CanonicalHostRedirectTest do
   use MembaWeb.ConnCase, async: true
 
+  alias MembaWeb.UserAuth
+
   test "redirects the Fly hostname to the canonical domain and preserves path and query", %{
     conn: conn
   } do
@@ -17,6 +19,7 @@ defmodule MembaWeb.Plugs.CanonicalHostRedirectTest do
     conn =
       conn
       |> Map.put(:host, "memba.io")
+      |> init_test_session(%{UserAuth.identity_session_key() => "pat@memba.io"})
       |> get("/admin/clubs")
 
     assert html_response(conn, 200) =~ "Clubs"
